282dad78e0724419a58ca15dda9f5ad75b705ca0
Lakshika Parihar <31981299+lakshikaparihar@users.noreply.github.com>
2021-05-14 17:47:51 +0530

    Delete main.py

--- file_download/main.py
+++
@@ -1,8 +0,0 @@
-import main_pb2
-import sys
-
-file = main_pb2.filepathdownload();
-file.path = input()
-print(main_pb2.filepathupload(downloadpath=file.path))
-details = main_pb2.filepathupload()
-print(details.downloadpath)
