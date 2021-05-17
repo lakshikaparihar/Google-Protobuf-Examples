6241171f11812de6f53e0f55f376f1df24243838
unknown <pariharlakshika@gmail.com>
2021-05-17 17:07:32 +0530

    update with gcp public url

--- file_download/server.py
+++ file_download/server.py
@@ -4,14 +4,29 @@ import main_pb2
 import main_pb2_grpc
 import urllib.request
 import os
-
+from urllib.parse import urlparse
+from google.cloud import storage
 
 
 class Downloader(main_pb2_grpc.DownloaderServicer):
 
+    def googleCloud(self,url):
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "cosmic-envoy-313907-2fc47918d007.json"
+        p = urlparse(url)
+        path = p.path[1:].split('/', 1)
+        bucket, file_path = path[0], path[1] 
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket)
+        blob = bucket.blob(file_path)
+        blob.download_to_filename(os.path.basename(file_path))
+        return os.path.abspath(file_path)
+
+
     def Download(self, request, context):
-        urllib.request.urlretrieve(request.fpath,request.fname)
-        self.lpath = os.path.abspath(request.fname)
+        if request.fpath.startswith("https://storage.googleapis.com"):
+            self.lpath=self.googleCloud(request.fpath)
+        #urllib.request.urlretrieve(request.fpath,request.fname)
+        #self.lpath = os.path.abspath(request.fname)
         return main_pb2.FilePathUpload(dpath=self.lpath)
 
 
